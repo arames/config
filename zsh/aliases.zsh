de48030f177094aa86e10cc1ea6654e0c7b29abc
Alexandre Rames <alexandre.rames@uop.re>
2014-03-31 13:35:04 +0100

    Add a file containing zsh aliases.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,5 @@
+alias grep='grep --color=auto --exclude=".tags" --exclude-dir=".git"'
+alias ds='diffstat'
+
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
