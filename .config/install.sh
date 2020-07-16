e6773168c1e029d5233fc779beaaf7485c45e7e0
Alexandre Rames <alexandre.rames@uop.re>
2020-07-16 12:42:26 -0700

    Clean install.sh.

--- .config/install.sh
+++ .config/install.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-GIT_PATH="$HOME/.config/config"
+CONFIG_REPO_PATH="$HOME/.config/config"
 
-git clone --bare git@github.com:arames/config.git $GIT_PATH
+git clone --bare git@github.com:arames/config.git $CONFIG_REPO_PATH
 function config {
-   /usr/bin/git --git-dir="$GIT_PATH/" --work-tree=$HOME $@
+  /usr/bin/git --git-dir="$CONFIG_REPO_PATH/" --work-tree=$HOME $@
 }
 
 DATE=`date +%F_%R`
 export BACKUP_DIR=$HOME/.config/backup/$DATE
-
 mkdir -p "$BACKUP_DIR"
+
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files to $BACKUP_DIR.";
-    config checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p $(dirname "$BACKUP_DIR"/{}); mv "$HOME/{}" "$BACKUP_DIR"/{};'
+else
+  echo "Backing up pre-existing dot files to $BACKUP_DIR.";
+  config checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p $(dirname "$BACKUP_DIR"/{}); mv "$HOME/{}" "$BACKUP_DIR"/{};'
 fi;
 config checkout
 config config status.showUntrackedFiles no
-
