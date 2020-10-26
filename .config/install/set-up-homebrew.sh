ea489204925bda19f876fea49a32f9be39f68088
Alexandre Rames <alexandre.rames@uop.re>
2020-10-26 11:46:45 -0700

    Add an installation file for homebrew recipes.

---
+++ .config/install/set-up-homebrew.sh
@@ -0,0 +1,32 @@
+#!env bash
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install \
+ctags \
+git \
+htop \
+nvim \
+stgit \
+tmux \
+zsh \
+
+# Install and symlink a few LLVM/Clang utilties.
+brew install llvm
+llvm=$(ls -d /usr/local/Cellar/llvm/* | head -n1)
+llvm_bins="$llvm/bin"
+for bin in clang-format git-clang-format; do
+  ln -s "$llvm_bins/$bin" /usr/local/bin/$bin
+done
+
+# Install yabai.
+# https://github.com/koekeishiya/yabai
+brew install koekeishiya/formulae/yabai
+sudo yabai --install-sa
+brew services start yabai
+killall Dock
+
+# Install skhd.
+# https://github.com/koekeishiya/skhd
+brew install koekeishiya/formulae/skhd
+brew services start skhd
